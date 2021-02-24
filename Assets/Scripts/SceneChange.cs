31f48d98ee8a38859510007f39dea5400f5fc6eb
MarconiGama <78350527+MarconiGama@users.noreply.github.com>
2021-02-24 16:20:26 -0400

    código de uma aplicação de RA com Unity e Vuforia
    Mecanismo de funcionamento de uma aplicação de realidade aumentada utilizando imagens escaneadas como ancora para projeção de imagens relacionadas.

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+    //declaração de variável do tipo string
+    public string sceneName;
+
+    //função para ir para RA
+    public void sChange()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    //função para sair do jogo
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
